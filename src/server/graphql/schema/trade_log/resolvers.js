373f8ad07b91de33c29dab04a3d189d276d6c1f3
kevin <kevin@iMac.local>
2018-09-13 09:36:05 +0800

    optimize fetch

--- src/server/graphql/schema/trade_log/resolvers.js
+++ src/server/graphql/schema/trade_log/resolvers.js
@@ -103,24 +103,35 @@ const TRADING_DAY = async(root,args,ctx,info) => {
 
 /*sina source with scale=240 does not include the T+0 Day's data, but could use 5 mins */
 const TRADING_DAY = async(root,args,ctx,info) => {
-  let num_at_scale = args.num * 60;
-  const url = 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sh000001&scale=5&ma=no&datalen=' + num_at_scale.toString();
-  const response = await axios.get(url);
-  let data = response.data;
-  data = data.replace(/day/g,'"day"').replace(/open/g,'"open"').replace(/high/g,'"high"').replace(/low/g,'"low"').replace(/close/g,'"close"').replace(/volume/g,'"volume"');
-  let a = JSON.parse(data);
-  let result = [];
-  let temp_str = "";
-  for (let idx =0; idx < a.length ; idx ++) {
-      let item = a[idx];
-      let temp_arr = item.day.split(" ");
-      if (temp_arr[0] != temp_str ){
-        temp_str = temp_arr[0];
-        result.push(temp_str);
-      }
+  let TempDateNow = new Date();
+  let DateNow = new Date(Math.floor(TempDateNow / 86400000) * 86400000);
+  let Last_date = new Date("2018-09-01");
+  if ( trading_day_list.length > 1 ) {
+    Last_date = new Date(trading_day_list[trading_day_list.length-1]);
   }
+  if  ((DateNow - Last_date != 0 ) || (trading_day_list.length < args.num)) {
+    //console.log("Fetch New data array",DateNow,Last_date);
+        let num_at_scale = args.num * 60;
+        const url = 'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol=sh000001&scale=5&ma=no&datalen=' + num_at_scale.toString();
+        const response = await axios.get(url);
+        let data = response.data;       
+        data = data.replace(/day/g,'"day"').replace(/open/g,'"open"').replace(/high/g,'"high"').replace(/low/g,'"low"').replace(/close/g,'"close"').replace(/volume/g,'"volume"');
+        
+        let a = JSON.parse(data);
+        let result = [];
+        let temp_str = "";
+        for (let idx =0; idx < a.length ; idx ++) {
+            let item = a[idx];
+            let temp_arr = item.day.split(" ");
+            if (temp_arr[0] != temp_str ){
+              temp_str = temp_arr[0];
+              result.push(temp_str);
+            }
+        }
+        trading_day_list = result
+  }  
 
-  return result.slice(result.length-args.num ,result.length );
+  return trading_day_list.slice(trading_day_list.length-args.num ,trading_day_list.length );
   /*
   let resulta = a.map((item)=>{ 
       return item.day; 
